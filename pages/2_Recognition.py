d63535f13768894dc6b6c9c122c46d8d61963a45
DM2523 <dsingh1@ce.iitr.ac.in>
2024-09-15 20:19:34 +0530

    initial commit

--- pages/2_Recognition.py
+++ pages/2_Recognition.py
@@ -174,7 +174,7 @@ def main():
                     # Sharpen the image 
                     # image_arr = cv2.filter2D(image_arr, -1, kernel)
                     image_arr = dynamic_resize(image_arr,target_size=800)
-                    pred_img,person_boxes = model.predict(image_arr)
+                    pred_img,person_boxes = model.predict(image_arr_copy)
                     pred_img_obj = Image.fromarray(pred_img)
                     # st.image(pred_img_obj)
                     if(len(person_boxes)!=0):
